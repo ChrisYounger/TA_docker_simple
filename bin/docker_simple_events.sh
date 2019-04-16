281467d7b0f5c205f3a1367c8a7740aad8dae09d
ChrisYounger <chris.younger@jds.net.au>
2019-04-16 21:41:03 +1000

    Changesgit status

--- bin/docker_simple_events.sh
+++ bin/docker_simple_events.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker events | grep -v "container top\|container exec_start\|container exec_die\|container exec_create"
+docker events | grep --line-buffered -v "container top\|container exec_start\|container exec_die\|container exec_create"
--- bin/docker_simple_ps.sh
+++ bin/docker_simple_ps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 command_date=$(date +%s)
-docker ps --format "$command_date\t{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Mounts}}\t{{.Networks}}\t{{.Size}}"
+docker ps -a --format "$command_date\t{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}\t{{.Mounts}}\t{{.Networks}}\t{{.Size}}"
