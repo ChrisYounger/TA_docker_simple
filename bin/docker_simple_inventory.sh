01a63b6a2300ff12790f214462024b9a6d11b676
ChrisYounger <chris.younger@gmail.com>
2021-02-08 11:52:04 +0000

    Update docker_simple_inventory.sh script so it no longer includes environment variables (becuase they often include plain text passwords) (Thank you securitypimp)

--- bin/docker_simple_inventory.sh
+++ bin/docker_simple_inventory.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker inspect $(docker ps -aq) | jq -c -M -r ".[]"
+# docker inspect $(docker ps -aq) | jq -c -M -r ".[]"
+docker inspect $(docker ps -aq) | jq -c -M -r '.[] |delpaths([paths | select(.[-1] | strings | startswith("Env"))])'
